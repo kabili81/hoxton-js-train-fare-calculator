3e3508a798e0ad1d960740b367192d7a0e788296
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-14 15:30:50 +0200

    update

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let myName = 'Kabil'
-console.log(myName)
+//- Ask the user for their age and how far they are travelling (in km)
+ // -- The price per travelled km will be £0.21
+//   -- Junior passengers (under 18) get a 20% discount
+  // -- Senior passengers (over 65) get a 40% discount
+//- Based on the answers, print a message in the console which tells the user how much they will need to pay for the ride.
+let myAge = prompt('How old are you?')
+console.log(myAge)
